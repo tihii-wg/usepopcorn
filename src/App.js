f6ce8f988faa7a4f6d00fc183dbdc55b756292a6
Vova <podolian@gmail.com>
2024-03-05 22:13:50 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
-		  hello
+      <Navbar />
     </div>
   );
 }
 
 export default App;
 
-
+function Navbar() {
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+      <div>
+        <input className="search" type="text" placeholder="Search movies..." />
+      </div>
+      <div>Found X results</div>
+    </nav>
+  );
+}
